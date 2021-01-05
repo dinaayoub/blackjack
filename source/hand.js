b6368640cfec4c46e7b82d7e3e832923f93167e9
Dina Ayoub <dinaayoub@hotmail.com>
2021-01-04 20:40:24 -0800

    implemented bet, did some formatting cleanup of requirements.md, and added a player test

--- source/hand.js
+++ source/hand.js
@@ -1,7 +1,7 @@
 'use strict';
 
-class Hand{
-  constructor(player){
+class Hand {
+  constructor(player) {
     this.cards = [];
     this.player = player;
     this.bet = 0;
@@ -9,37 +9,36 @@ class Hand{
     this.count;
   }
 
-  addCard(card){
+  addCard(card) {
     this.cards.push(card);
   }
 
-  
-  totalHandCount(){
-    for(let i = 0; i < this.cards.length; i++)
-      if (this.cards[i].rank === 'A'){
+  totalHandCount() {
+    for (let i = 0; i < this.cards.length; i++)
+      if (this.cards[i].rank === 'A') {
         this.cards[i].delete();
         this.cards[i].push();
         i--;
       }
-    this.cards.forEach(card =>{
+    this.cards.forEach(card => {
       if (card.rank === Number) {
         card.value = card.rank;
         this.count += card.value;
       }
-      if ((card.rank ==='J')||(card.rank ==='Q')||(card.rank ==='K')){
+      if ((card.rank === 'J') || (card.rank === 'Q') || (card.rank === 'K')) {
         card.value = 10;
         this.count += card.value;
 
       }
-      if ((card.rank === 'A') && (this.count < 11)){
+      if ((card.rank === 'A') && (this.count < 11)) {
         card.value = 11;
         this.count += card.value;
       }
-      if ((card.rank === 'A') && (this.count >= 11)){
+      if ((card.rank === 'A') && (this.count >= 11)) {
         card.value = 1;
         this.count += card.value;
       }
-      return  this.count;
+      return this.count;
     });
   }
 }
